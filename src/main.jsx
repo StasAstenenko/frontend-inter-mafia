916407e8d861aa61e2df96a69371421216b73660
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2024-11-24 14:50:02 +0100

    localisation files

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
+import "./locales/i18n.js";
+import { LanguageProvider } from "./locales/langContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <App />
+          <LanguageProvider>
+            <App />
+          </LanguageProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
