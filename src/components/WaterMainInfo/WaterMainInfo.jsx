69d8d8b0c28760eb2639240b2f5abc2f67d85726
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-11-24 10:15:16 +0200

    up

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,5 +1,31 @@
+import css from "./WaterMainInfo.module.css";
+
 const WaterMainInfo = () => {
-  return <></>;
+  return (
+    <>
+      <div className={css.container}>
+        <h2 className={css.title}>aquatrack</h2>
+        <picture>
+          <source
+            srcSet="
+                    ./img/projects/project-1-desc.png    1x,
+                    ./img/projects/project-1-desc@2x.png 2x
+                  "
+            media="(min-width: 768px)"
+          />
+
+          <source
+            srcSet="
+                    ./img/projects/project-1-mob.png    1x,
+                    ./img/projects/project-1-mob@2x.png 2x
+                  "
+            media="(max-width: 767px)"
+          />
+          <img src="./img/projects/project-1-mob.png" alt="First project" />
+        </picture>
+      </div>
+    </>
+  );
 };
 
 export default WaterMainInfo;
