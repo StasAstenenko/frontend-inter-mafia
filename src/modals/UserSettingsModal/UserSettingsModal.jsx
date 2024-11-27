951d4cb7e7267f301dcfabd3ced5a3e61db98502
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-11-27 02:24:05 +0100

    add styles user setting modal

--- src/modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/modals/UserSettingsModal/UserSettingsModal.jsx
@@ -9,16 +9,21 @@ const UserSettingsModal = ({ isOpen, onClose }) => {
   return ReactDOM.createPortal(
     <div className={css.settingModalOverlay}>
       <div className={css.settingModal}>
-        <h2 className={css.settingModalTitle}>Setting</h2>
-        <button className={css.settingModalButton} onClick={onClose}>
-          <Icon
-            iconName="close"
-            width="24"
-            height="24"
-            className="css.settingModalIconClose"
-          />
-        </button>
-        <UsersSettingsForm onClose={onClose} />
+        <div className={css.settingModalHeader}>
+          <h2 className={css.settingModalTitle}>Setting</h2>
+          <button className={css.settingModalButton} onClick={onClose}>
+            <Icon
+              iconName="close"
+              width="24"
+              height="24"
+              className="css.settingModalIconClose"
+            />
+          </button>
+        </div>
+
+        <div className={css.settingModalContent}>
+          <UsersSettingsForm onClose={onClose} />
+        </div>
       </div>
     </div>,
     document.body
