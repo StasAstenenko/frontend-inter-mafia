dd10d618ea508fc1e1d384537ffa11c54e7cfeae
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-11-28 00:22:02 +0100

    create logic for setting modal

--- src/modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/modals/UserSettingsModal/UserSettingsModal.jsx
@@ -1,13 +1,31 @@
 import ReactDOM from "react-dom";
 import UsersSettingsForm from "../../components/UsersSettingsForm/UsersSettingsForm";
 import css from "./UserSettingsModal.module.css";
+import { useEffect } from "react";
 
-const UserSettingsModal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+const UserSettingsModal = ({ onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
 
   return ReactDOM.createPortal(
-    <div className={css.settingModalOverlay}>
-      <div className={css.settingModal}>
+    <div className={css.settingModalOverlay} onClick={handleOverlayClick}>
+      <div className={css.settingModal} onClick={(e) => e.stopPropagation()}>
         <div className={css.settingModalHeader}>
           <h2 className={css.settingModalTitle}>Setting</h2>
           <button className={css.settingModalButton} onClick={onClose}>
