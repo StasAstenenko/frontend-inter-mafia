310d3d70dc04c901cc83c50ae4a887e5d58cf0f8
Sergey Stupak <itstupak@gmail.com>
2024-11-27 12:04:44 +0100

    add modal windows (main, logout, delete)

--- src/modals/LogOutModal/LogOutModal.jsx
+++ src/modals/LogOutModal/LogOutModal.jsx
@@ -1,49 +1,41 @@
-import Modal from "react-modal";
 import css from "./LogOutModal.module.css";
-Modal.setAppElement("#root");
 
-const LogOutModal = ({ isOpen, onRequestClose, onConfirm, title, message }) => {
-  const customStyles = {
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.3)",
-    },
-    content: {
-      position: "absolute",
-      top: "20%",
-      left: "auto",
-      right: "42%",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      padding: "0",
-      border: "none",
-      maxWidth: "100%",
-      maxHeight: "100%",
-      height: "auto",
-      overflow: "hidden",
-    },
-  };
+// import { useDispatch } from "react-redux";
+// import { logOut } from "../../redux/auth/operations";
+
+const LogOutModal = () => {
+  // const dispatch = useDispatch();
+  // const closeModal = ? ;
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      className={css.modal}
-      overlayClassName={css.overlay}
-      style={customStyles}
-    >
-      <div className={css.container}>
-        <h2>{title}</h2>
-        <p>{message}</p>
-        <div className={css.btns}>
-          <button className={css.confirmBtn} onClick={onConfirm}>
-            Yes, confirm
-          </button>
-          <button className={css.cancelBtn} onClick={onRequestClose}>
-            Cancel
-          </button>
-        </div>
+    <div className={css.modalWindow}>
+      <div className={css.wrapperText}>
+        <h2 className={css.title}>Log Out</h2>
+        <p className={css.text}>Do you really want to leave?</p>
       </div>
-    </Modal>
+      <div className={css.buttonContainer}>
+        <button
+          className={css.logoutBtn}
+          type="button"
+          aria-label="Logout button"
+          // onClick={() => {
+          //   dispatch(logOut());
+          //   closeModal();
+          // }}
+          // Плюс не забыть оповещение с помощью Toast
+        >
+          Log Out
+        </button>
+        <button
+          className={css.cancelBtn}
+          type="button"
+          aria-label="Cancel button"
+          // onClick={closeModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/modals/Modal/Modal.jsx
+++ src/modals/Modal/Modal.jsx
@@ -1,5 +1,42 @@
-const Modal = () => {
-  return <></>;
+import { useEffect } from "react";
+import css from "./Modal.module.css";
+import { icons as sprite } from "../../icons/index";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscape);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, onClose]);
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <div className={css.backdrop} onClick={onClose}>
+      <div className={css.modalWindow} onClick={(e) => e.stopPropagation()}>
+        <button
+          type="button"
+          aria-label="Close modal button"
+          className={css.closeBtn}
+          onClick={onClose}
+        >
+          <svg className={css.closeIcon}>
+            <use xlinkHref={`${sprite}#close`} />
+          </svg>
+        </button>
+
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
