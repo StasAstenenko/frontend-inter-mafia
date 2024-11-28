72836f3205d579193e1d2be0e4c3374ce30e8f25
ElmiraEminova <153520598+ElmiraEminova@users.noreply.github.com>
2024-11-28 07:47:44 +0100

    add work to UserPanel, WDI,UserBar,UserPopover

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,5 +1,37 @@
-const UserBarPopover = () => {
-  return <></>;
-};
+import { forwardRef } from "react"
+import css from "./UserBarPopover.module.css"
+
+
+const UserBarPopover = forwardRef(
+  ({ styles, attributes, onSettingsClick, onLogOutClick, closePopover }, ref) => {
+    return (
+      <div
+        ref={ref}
+        style={styles.popper}
+        {...attributes.popper}
+        className={css.userbarpopover}
+      >
+        <button
+          className={css.popoverbutton}
+          onClick={() => {
+            onSettingsClick();
+            closePopover();
+          }}
+        >
+          Settings
+        </button>
+        <button
+          className={css.popoverbutton}
+          onClick={() => {
+            onLogOutClick();
+            closePopover();
+          }}
+        >
+          Log out
+        </button>
+      </div>
+    );
+  }
+);
 
 export default UserBarPopover;
--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,43 @@
-const UserPanel = () => {
-  return <></>;
+import  { useState } from "react";
+import UserBar from "../UserBar/UserBar.jsx";
+import UserSettingsModal from "../UserSettingsModal/UserSettingsModal.jsx";
+import LogOutModal from "../LogOutModal/LogOutModal.jsx";
+import css from "./UserPanel.module.css"
+
+const UserPanel = ({ userName, avatarUrl }) => {
+  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+  const [isLogOutModalOpen, setIsLogOutModalOpen] = useState(false);
+
+  const handleOpenSettings = () => {
+  console.log("Settings modal opened");
+  setIsSettingsModalOpen(true);
+};
+const handleOpenLogOut = () => {
+  console.log("Log out modal opened");
+  setIsLogOutModalOpen(true);
+};
+  const handleCloseModals = () => {
+    setIsSettingsModalOpen(false);
+    setIsLogOutModalOpen(false);
+  };
+
+  return (
+    <div className={css.userpanel}> 
+      <h2 className={css.greeting}>Hello, <strong>{userName}!</strong></h2>
+      <UserBar
+        userName={userName}
+        avatarUrl={avatarUrl}
+        onSettingsClick={handleOpenSettings}
+        onLogOutClick={handleOpenLogOut}
+      />
+      {isSettingsModalOpen && (
+        <UserSettingsModal onClose={handleCloseModals} />
+      )}
+      {isLogOutModalOpen && (
+        <LogOutModal onClose={handleCloseModals} />
+      )}
+    </div>
+  );
 };
 
 export default UserPanel;
