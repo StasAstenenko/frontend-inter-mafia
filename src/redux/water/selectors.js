c9858c086ba735321564b0f6e6e2276c7d61e268
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-11-28 10:55:59 +0200

    dailyNorm/redux

--- src/redux/water/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectDailyNorma = (state) => state.water.dailyNorma;
-export const selectLoading = (state) => state.water.loading;
-export const selectError = (state) => state.water.error;
