f6c262b2b6a05b9286209a51232a66387cb88195
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-12-01 00:51:18 +0100

    add setting form

--- src/components/UsersSettingsForm/UsersSettingsForm.jsx
+++ src/components/UsersSettingsForm/UsersSettingsForm.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import css from "./UsersSettingsForm.module.css";
+import { useEffect, useState } from "react";
 
 const validationSettingSchema = Yup.object().shape({
   avatarUrl: Yup.mixed(),
@@ -24,6 +25,8 @@ const validationSettingSchema = Yup.object().shape({
 
 const UsersSettingsForm = () => {
   const [avatarPreview, setAvatarPreview] = useState(null);
+  const [calculateWaterNorm, setCalculateWaterNorm] = useState(null);
+
   const {
     register,
     handleSubmit,
@@ -37,6 +40,19 @@ const UsersSettingsForm = () => {
   const weight = watch("weight");
   const activeTime = watch("activeTime");
   const gender = watch("gender");
+
+  useEffect(() => {
+    if (weight && activeTime && gender) {
+      let waterNorm = 0;
+      if (gender === "woman") {
+        waterNorm = Math.max(weight * 0.03 + activeTime * 0.4, 0);
+      } else if (gender === "man") {
+        waterNorm = Math.max(weight * 0.04 + activeTime * 0.6, 0);
+      }
+      setValue("waterDrink", waterNorm.toFixed(1));
+      setCalculateWaterNorm(waterNorm.toFixed(1));
+    }
+  }, [weight, activeTime, gender, setValue]);
 
   const handleAvatarChange = (e) => {
     const file = e.target.files[0];
@@ -52,24 +68,112 @@ const UsersSettingsForm = () => {
       onSubmit={handleSubmit((d) => console.log(d))}
     >
       <div className={css.settingFormAvatar}>
-        <img className={css.settingAvatarImg} src={avatarPreview} />
-        <button
-          className={css.settingModalButton}
-          type="button"
-          onClick={() => document.getElementById("avatarInput").click()}
-        >
-          <svg width="18" height="18" className={css.settingAvatarIcon}>
-            <use href="/icons/sprite.svg#upload"></use>
-          </svg>
-        </button>
-        <input
-          type="file"
-          id="avatarInput"
-          {...register("avatar")}
-          onChange={handleAvatarChange}
-          style={{ display: "none" }}
-        />
+        {avatarPreview ? (
+          <img
+            className={css.settingAvatarImg}
+            src={avatarPreview}
+            alt="Avatar"
+          />
+        ) : (
+          <div className={css.avatarPlaceholder}>N</div>
+        )}
+        <div>
+          <button
+            className={css.settingModalButton}
+            type="button"
+            onClick={() => document.getElementById("avatarInput").click()}
+          >
+            <svg width="18" height="18" className={css.settingAvatarIcon}>
+              <use href="/icons/sprite.svg#upload"></use>
+            </svg>
+
+            <span className={css.avatarButtonText}>Upload a photo</span>
+          </button>
+          <input
+            type="file"
+            id="avatarInput"
+            {...register("avatar")}
+            onChange={handleAvatarChange}
+            style={{ display: "none" }}
+          />
+        </div>
       </div>
+      <div className={css.settingAllForms}>
+        <div className={css.settingGenderForm}>
+          <label>Your gender identity</label>
+          <label>
+            <input type="radio" value="woman" {...register("gender")} />
+            Woman
+          </label>
+          <label>
+            <input type="radio" value="man" {...register("gender")} />
+            Man
+          </label>
+          {errors.gender && <p>{errors.gender.message}</p>}
+        </div>
+        <div className={css.settingNameForm}>
+          <div>
+            <label>Your name</label>
+            <input type="text" {...register("name")} />
+            {errors.name && <p>{errors.name.message}</p>}
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" {...register("email")} />
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
+        </div>
+        <div className={css.settingDailyForm}>
+          <label>My daily norma</label>
+          <div>
+            <p>
+              For woman: <span>V=(M*0.03) + (T*0.4)</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              For man: <span>V=(M*0.04) + (T*0.6)</span>
+            </p>
+          </div>
+          <div>
+            <p>
+              <span>*</span> * V is the volume of the water norm in liters per
+              day, M is your body weight, T is the time of active sports, or
+              another type of activity commensurate in terms of loads (in the
+              absence of these, you must set 0)
+            </p>
+          </div>
+          <div>
+            <span>!</span> Active time in honours
+          </div>
+        </div>
+        <div className={css.settingWeightTimeForm}>
+          <div>
+            <label>Your weight in kilograms:</label>
+            <input type="number" {...register("weight")} />
+            {errors.weight && <p>{errors.weight.message}</p>}
+          </div>
+          <div>
+            <label>The time of active participation in sports:</label>
+            <input type="number" {...register("activeTime")} />
+            {errors.activeTime && <p>{errors.activeTime.message}</p>}
+          </div>
+        </div>
+        <div className={css.settingCalculateForm}>
+          <div>
+            <p>The required amount of water in liters per day:</p>
+            <span>{calculateWaterNorm}</span>
+          </div>
+          <div>
+            <label>Write down how much water you will drink:</label>
+            <input type="number" {...register("waterNorm")} disabled />
+            {errors.waterNorm && <p>{errors.waterNorm.message}</p>}
+          </div>
+        </div>
+      </div>
+
+      <button type="submit">Save</button>
     </form>
   );
 };
