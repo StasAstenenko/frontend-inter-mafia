8d35f4af57068d6963111747777de51b3c548d02
Anton Pokydko <AntonPokydko@gmail.com>
2024-12-01 11:48:42 +0200

    use conteiner for WaterDetailedInfo & some oter styles for calendar

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,15 +1,16 @@
 import UserPanel from "../../components/UserPanel/UserPanel.jsx";
 import DailyInfo from "../DailyInfo/DailyInfo.jsx";
 import MonthInfo from "../../components/MonthInfo/MonthInfo.jsx";
+import Container from "../../components/Container/Container.jsx";
 import css from "./WaterDetailedInfo.module.css";
 
 const WaterDetailedInfo = () => {
   return (
-    <div className={css.container}>
+    <Container className={css.container}>
       <UserPanel />
       <DailyInfo />
       <MonthInfo />
-    </div>
+    </Container>
   );
 };
 
